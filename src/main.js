934aa7f67d4cb7af3fc5a199d5dc4108e8cd7dbb
Seiji Ikeda <seiji0426.32@i.softbank.jp>
2021-03-20 22:40:07 +0900

    practice componemt

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Like from './components/Like.vue'
 
 Vue.config.productionTip = false
+Vue.component('Like',Like)
 
 new Vue({
   render: h => h(App),
